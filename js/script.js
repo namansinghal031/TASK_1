e8dd54d206dd2617333f2fc4f140f346b1f7eb28
namansinghl031 <111794070+namansinghal031@users.noreply.github.com>
2024-07-08 21:50:27 +0530

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,5 @@
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Form submitted successfully!');
+    // Here, you can add your form submission logic (e.g., AJAX request)
+});
